9e6793a11bfc007885fda1032652d63b6b78a7e3
jyomama28 <jyomama28@gmail.com>
2025-08-23 21:33:12 -0400

    Add files via upload

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+// primitive varible = stored directly in memory. (stack)
+// reference varible = memory address. (heap)
