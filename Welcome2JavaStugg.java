4a8e1c6eaf4f4e99ad10600f26b0a235ae69f860
jyomama28 <jyomama28@gmail.com>
2025-09-04 17:05:41 -0400

    Add files via upload

---
+++ Welcome2JavaStugg.java
@@ -0,0 +1,12 @@
+public class Welcome2JavaStugg {
+    public class idontknow {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Stugg By Jyomama28!");
+        System.out.println("This is a way for me to learn java!");
+        System.out.println("Im bored an cant find anything else to make!");
+        System.out.println("if you have any ideas on what I could make, Please send me an email at: jyomama28@gmail.com")
+
+        }
+    }
+
+}
