6966275b73d9121b87440038efd8a503db0e11fb
jyomama28 <jyomama28@gmail.com>
2025-10-16 14:23:11 -0400

    Add files via upload

---
+++ SeasonFinder.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class SeasonFinder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int month = scanner.nextInt();
+        String season = "";
+        
+        switch (month) {
+            case 12:
+            case 1:
+            case 2:
+                season = "Winter";
+                break;
+            case 3:
+            case 4:
+            case 5:
+                season = "Spring";
+                break;
+            case 6:
+            case 7:
+            case 8:
+                season = "Summer";
+                break;
+            case 9:
+            case 10:
+            case 11:
+                season = "Autumn";
+                break;
+            default:
+                season = "Invalid month";
+        }
+        
+        System.out.println(season);
+        scanner.close();
+    }
+}
