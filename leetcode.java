4020eb407cb659c3f4421f9e613779c5677edc8d
jyomama28 <jyomama28@gmail.com>
2025-10-15 20:12:37 -0400

    Add files via upload
    a basic array in java

---
+++ leetcode.java
@@ -0,0 +1,11 @@
+public class leetcode {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 4;
+        int c = 3;
+
+        String[] arr = {"2", "4", "3"};
+
+        System.out.println(java.util.Arrays.toString(arr));
+    }
+}
