fd4a2e11f1b02f0a055b3810401d2473f3e5c9fe
jyomama28 <jyomama28@gmail.com>
2025-10-31 17:02:40 -0400

    Add ToDos class with ArrayList example

---
+++ arraylist2.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+class ToDos {
+    
+  public static void main(String[] args) {
+    
+    ArrayList<String> toDoList = new ArrayList<String>();
+    String toDo1 = "Water plants";
+    // lol very nice names
+    String toDo2 = "Plants Water";
+    String toDo3 =  "Pater Wlants";
+
+    toDoList.add(toDo1);
+    toDoList.add(toDo2);
+    toDoList.add(toDo3);
+    
+    System.out.println(toDoList);
+      
+    
+  }
+  
+}
