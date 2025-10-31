604bcdb3bb64f8c25a27cbd6138edb08ea14d41f
jyomama28 <jyomama28@gmail.com>
2025-10-31 16:56:49 -0400

    Create ToDos class with ArrayList initialization
    Initialized a basic ToDos class with an ArrayList for a to-do list.

---
+++ arraylist.java
@@ -0,0 +1,13 @@
+// this is just a personal reference for myself
+// use as you wish
+import java.util.ArrayList;
+
+class ToDos {
+  
+  public static void main(String[] args) {
+
+    ArrayList<String> toDoList = new ArrayList<String>();
+    
+  }
+  
+}
