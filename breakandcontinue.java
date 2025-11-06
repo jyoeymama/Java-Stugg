5b4afba132bab42713da52fa25304f3c92e6a444
jyomama28 <jyomama28@gmail.com>
2025-11-05 20:24:39 -0500

    Add Numbers class to print multiples of 5

---
+++ breakandcontinue.java
@@ -0,0 +1,13 @@
+// thanks to everyone who looks through my code!
+// I appreciate my supporters and fans!
+// I must keep on coding, just... keep.. on.
+class Numbers {
+  public static void main(String[] args) {
+    for (int i = 0; i < 100; i++) {
+      if (i % 5 != 0) {
+        continue;
+      }
+      System.out.println(i);
+    }
+  }
+}    
